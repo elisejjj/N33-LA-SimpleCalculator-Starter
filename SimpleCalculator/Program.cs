86094d3fd80297cc02c58603d6f7381516eaac9f
XavierBradley <xavbrdl@gmail.com>
2025-09-21 14:32:42 -0400

    Made it a loop for fault tolerance

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -8,49 +8,59 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            try
+            double calculatorOn = 1;
+            while (calculatorOn != 2)
             {
-                // Class to convert user input
-                InputConverter inputConverter = new InputConverter();
 
-                // Class to perform actual calculations
-                CalculatorEngine calculatorEngine = new CalculatorEngine();
 
-                Console.WriteLine("Write the first number:");
-                
-                double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
 
-                Console.WriteLine("Write the second number:");
-                double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
-
-                Console.WriteLine("Write the operation (+, -, /, *):");
-                string operation = Console.ReadLine();
-
-                if (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+                try
                 {
-                    Console.WriteLine("Invalid operator. Type in +, -, /, or * without any spaces or extra characters.");
-                    operation = Console.ReadLine();
+                    // Class to convert user input
+                    InputConverter inputConverter = new InputConverter();
+
+                    // Class to perform actual calculations
+                    CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                    Console.WriteLine("Write the first number:");
+
+                    double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+
+                    Console.WriteLine("Write the second number:");
+                    double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+
+                    Console.WriteLine("Write the operation (+, -, /, *):");
+                    string operation = Console.ReadLine();
+
+                    if (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+                    {
+                        Console.WriteLine("Invalid operator. Type in +, -, /, or * without any spaces or extra characters.");
+                        operation = Console.ReadLine();
+                    }
+
+                    double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                    StringBuilder finalMessage = new StringBuilder("The value ");
+                    finalMessage.Append(firstNumber.ToString());
+                    finalMessage.Append(" ");
+                    finalMessage.Append(operation.ToString());
+                    finalMessage.Append(" the value ");
+                    finalMessage.Append(secondNumber.ToString());
+                    finalMessage.Append(" is equal to ");
+                    finalMessage.Append(result.ToString("F2"));
+
+                    Console.WriteLine(finalMessage.ToString());
+
+                    Console.WriteLine("Enter 1 to do another calculation, or 2 to close the calculator");
+                    calculatorOn = InputConverter.ConvertInputToNumeric(Console.ReadLine());
                 }
-
-                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
-                
-                StringBuilder finalMessage =  new StringBuilder("The value ");
-                finalMessage.Append(firstNumber.ToString());
-                finalMessage.Append(" ");
-                finalMessage.Append(operation.ToString());
-                finalMessage.Append(" the value ");
-                finalMessage.Append(secondNumber.ToString());
-                finalMessage.Append(" is equal to ");
-                finalMessage.Append(result.ToString("F2")); 
-
-                Console.WriteLine(finalMessage.ToString());
-
-            } catch (FormatException ex)
-            {
-                // Normally, we'd log this error to a file.
-                Console.WriteLine("Invalid input. Please input a single number without any spaces or extra characters.");
+                catch (FormatException ex)
+                {
+                    // Normally, we'd log this error to a file.
+                    Console.WriteLine("Invalid input. Please input a single number without any spaces or extra characters.");
+                }
+                 
             }
-
         }
     }
 }

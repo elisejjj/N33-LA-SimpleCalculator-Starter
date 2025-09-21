7f366c7707a9dbc1fbcbc2ed23ac35b8308caab5
XavierBradley <xavbrdl@gmail.com>
2025-09-21 15:29:38 -0400

    shifted calculatorengine around to be viable

--- CalculatorEngine/CalculatorEngine.cs
+++ CalculatorEngine/CalculatorEngine.cs
@@ -3,35 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace CalculatorEngine
+
+namespace CalculatorEngineName
 {
     public class CalculatorEngine
     {
-            public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
-            {
-                double result = 0;
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result = 0;
 
-                if (argOperation == "+"  || argOperation.ToLower() == "add")
+            if (argOperation == "+" || argOperation.ToLower() == "add")
             {
-                    result = argFirstNumber + argSecondNumber;
-                }
-            else if (argOperation == "-"  || argOperation.ToLower() == "minus")
-            {
-                    result = argFirstNumber - argSecondNumber;
-                }
-            else if (argOperation == "*" || argOperation.ToLower() == "multiply")
-                {
-                    result = argFirstNumber * argSecondNumber;
-                }
-                else if (argOperation == "/" || argOperation.ToLower() == "divide")
-                {
-                    result = argFirstNumber / argSecondNumber;
-                }
-
-                return result;
+                result = argFirstNumber + argSecondNumber;
             }
+            else if (argOperation == "-" || argOperation.ToLower() == "minus")
+            {
+                result = argFirstNumber - argSecondNumber;
+            }
+            else if (argOperation == "*" || argOperation.ToLower() == "multiply")
+            {
+                result = argFirstNumber * argSecondNumber;
+            }
+            else if (argOperation == "/" || argOperation.ToLower() == "divide")
+            {
+                result = argFirstNumber / argSecondNumber;
+            }
+
+            return result;
         }
-    }
-
-
-
+}
---
+++ CalculatorEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CalculatorEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorEngine")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8cd48deb-97ce-4397-bd51-2055c4ac60ae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
